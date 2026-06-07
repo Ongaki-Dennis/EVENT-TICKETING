cf8b493755a2c3aa3d40814229ba2e189009306c
Ongaki-Dennis <ongakidennis12@gmail.com>
2026-06-07 17:33:03 +0300

    Remove obsolete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-try {
-  const compiled = require("./dist/src/server.js");
-  module.exports = compiled.default || compiled;
-} catch (error) {
-  require("tsx/cjs");
-  const source = require("./src/server.ts");
-  module.exports = source.default || source;
-}
